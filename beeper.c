2f7f1ad7482b62f66211fa2778127fef8714d238
Crux10086 <1609549472@qq.com>
2018-01-05 21:45:06 +0800

    蜂鸣器接口程序

---
+++ beeper.c
@@ -0,0 +1,30 @@
+#include<reg51.h>
+#include"beeper.h"
+
+
+sbit en = P1 ^ 7;
+sbit mode = P1 ^ 6;
+sbit sel = P1 ^ 5;
+
+void BEEPER_Set_Tune(signed char tune){
+	en = 1, mode = 0, sel = 0;
+	P0 = tune + 48;
+	en = 0;
+}
+
+void BEEPER_Set_Length(unsigned char length){
+	en = 1, mode = 0, sel = 1;
+	P0 = length;
+	en = 0;
+}
+
+void BEEPER_Stop(void){
+	en = 1, mode = 1, sel = 1;
+	en = 0;
+}
+
+void BEEPER_Start(void){
+	BEEPER_Stop();
+	en = 1, mode = 1, sel = 0;
+	en = 0;
+}
